ab62203f3e10e64f1d1687d1938007266405ed03
Bruno Sonnino <sonnino@revolution.com.br>
2017-03-18 08:15:28 -0300

    Add project files.

---
+++ Fibonacci.Tests/FibonacciTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fibonacci.Tests
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [DataRow(0, 0)]
+        [DataRow(1, 1)]
+        [DataRow(2, 1)]
+        [DataRow(80, 23416728348467685)]
+        [DataTestMethod]
+        public void GivenDataFibonacciReturnsResultsOk(int number, Int64 result)
+        {
+            var fib = new Fib();
+            var actual = fib.Fibonacci(number);
+            Assert.AreEqual(result, actual);
+        }
+
+    }
+}
---
+++ Fibonacci/Fib.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    public class Fib
+    {
+        public Int64 Fibonacci(int n)
+        {
+            Int64 a = 0;
+            Int64 b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n; i++)
+            {
+                Int64 temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+    }
+}
